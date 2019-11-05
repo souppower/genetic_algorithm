e7891c80d738f36511b2c5f9fefef617b5910751
souppower <r.kubota92@gmail.com>
2019-11-06 02:03:44 +0900

    calculateFitness

--- genetic_algorithm.go
+++ genetic_algorithm.go
@@ -19,10 +19,24 @@ func Evolve(target []byte) []byte {
 func createOrganizm(target []byte) *Organism {
 	text := generateRandomText(len(target))
 
-	return &Organism{
+	o := &Organism{
 		DNA:     text,
 		Fitness: 0,
 	}
+	o.calculateFitness(target)
+
+	return o
+}
+
+func (o *Organism) calculateFitness(target []byte) {
+	var match int
+	for i := range o.DNA {
+		if o.DNA[i] == target[i] {
+			match++
+		}
+	}
+
+	o.Fitness = float64(match) / float64(len(o.DNA))
 }
 
 func generateRandomText(length int) []byte {
--- genetic_algorithm_test.go
+++ genetic_algorithm_test.go
@@ -22,3 +22,30 @@ func TestCreatePopulation(t *testing.T) {
 		t.Errorf("Expected %d, but got %d", PopulationSize, res)
 	}
 }
+
+func TestCalculateFitness(t *testing.T) {
+	o := &Organism{
+		DNA: []byte("abdc"),
+	}
+
+	o.calculateFitness([]byte("abcd"))
+	expected := float64(0.5)
+
+	if o.Fitness != expected {
+		t.Errorf("Expected %f, but got %f", expected, o.Fitness)
+	}
+
+}
+
+func TestCalculateFitnessIdentical(t *testing.T) {
+	o := &Organism{
+		DNA: []byte("abcd"),
+	}
+
+	o.calculateFitness([]byte("abcd"))
+	expected := float64(1)
+
+	if o.Fitness != expected {
+		t.Errorf("Expected %f, but got %f", expected, o.Fitness)
+	}
+}
