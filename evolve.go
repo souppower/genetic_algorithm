3ecc8a3b3679bd5ef471655516d6859aeb45301a
souppower <r.kubota92@gmail.com>
2019-11-11 05:31:46 +0900

    reproduce

--- evolve.go
+++ evolve.go
@@ -1,6 +1,32 @@
 package genetic_algorithm
 
+import (
+	"math/rand"
+	"time"
+)
+
 func Evolve(target []byte) []byte {
 	NewOrganism(target)
 	return target
+}
+
+func reproduce(population []*Organism, target []byte) []*Organism {
+	nextGeneration := make([]*Organism, len(population))
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for i := range population {
+		r1 := rand.Intn(len(population))
+		r2 := rand.Intn(len(population))
+
+		parent1 := population[r1]
+		parent2 := population[r2]
+
+		child := parent1.crossOver(parent2, target)
+		child.mutate()
+
+		nextGeneration[i] = child
+	}
+
+	return nextGeneration
 }
--- evolve_test.go
+++ evolve_test.go
@@ -13,3 +13,29 @@ func TestEvolve(t *testing.T) {
 		t.Errorf("Expected %s, but got %s", target, res)
 	}
 }
+
+func TestReproduce(t *testing.T) {
+	target := []byte("abcd")
+	population := []*Organism{
+		{
+			DNA:     []byte("abcd"),
+			Fitness: 0.1,
+		},
+		{
+			DNA:     []byte("abdc"),
+			Fitness: 0,
+		},
+		{
+			DNA:     []byte("abxx"),
+			Fitness: 0.23,
+		},
+	}
+
+	res := reproduce(population, target)
+
+	if len(res) != len(population) {
+		t.Errorf("Expected %v, but got %v", len(population), len(res))
+	}
+
+}
+
