26b346f6ec5c6fc60eac3025701b29ac622d16e4
souppower <r.kubota92@gmail.com>
2019-11-14 09:01:48 +0900

    util method for testing fitness ratio in the pool

--- generation_test.go
+++ generation_test.go
@@ -23,6 +23,86 @@ func TestNewGeneration(t *testing.T) {
 	}
 }
 
+func TestContainsMoreHighFitnessThanLowerFitness(t *testing.T) {
+	oo := []*Organism{
+		{
+			Fitness: 0,
+		},
+		{
+			Fitness: 0.1,
+		},
+		{
+			Fitness: 0.2,
+		},
+		{
+			Fitness: 0.1,
+		},
+		{
+			Fitness: 0.2,
+		},
+		{
+			Fitness: 0.2,
+		},
+	}
+	expected := true
+
+	res := containsMoreHighFitnessThanLowerFitness(oo)
+
+	if res != expected {
+		t.Errorf("High fitness organisms should exceed lower fitness organisms. Expected %t, but got %t", expected, res)
+	}
+}
+
+func TestContainsMoreHighFitnessThanLowerFitnessInvalid(t *testing.T) {
+	oo := []*Organism{
+		{
+			Fitness: 0.1,
+		},
+		{
+			Fitness: 0.1,
+		},
+		{
+			Fitness: 0.2,
+		},
+		{
+			Fitness: 0.2,
+		},
+	}
+	expected := false
+
+	res := containsMoreHighFitnessThanLowerFitness(oo)
+
+	if res != expected {
+		t.Errorf("Expected %t, but got %t", expected, res)
+	}
+}
+
+func containsMoreHighFitnessThanLowerFitness(oo []*Organism) bool {
+	memo := make(map[int]int, 0)
+	for _, r := range oo {
+		ratio := int(r.Fitness * 100)
+		memo[ratio]++
+	}
+
+	keys := make([]int, 0)
+	for k := range memo {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	for i := 0; i < len(keys)-1; i++ {
+		comp := memo[keys[i]]
+		for j := i + 1; j < len(keys); j++ {
+			if memo[keys[j]] <= comp {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func TestGetBestFitness(t *testing.T) {
 	population := []*Organism{
 		{
